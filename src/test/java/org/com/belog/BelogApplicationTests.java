93d9ff3d286fb940814e3ccb52034d6c5580f74f
jyvnee <jyunee54@gmail.com>
2026-08-21 09:53:18 +0900

    Initial commit

---
+++ src/test/java/org/com/belog/BelogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.com.belog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
