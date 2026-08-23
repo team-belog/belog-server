feddf0a1c6e4591dfcee1a9235318103e6827486
jyvnee <jyunee54@gmail.com>
2026-08-24 00:18:28 +0900

    feat: 커스텀 예외 추가

---
+++ src/main/java/org/com/belog/global/error/BusinessException.java
@@ -0,0 +1,20 @@
+package org.com.belog.global.error;
+
+import lombok.Getter;
+import org.com.belog.global.response.code.ErrorCode;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public BusinessException(ErrorCode errorCode, Throwable cause) {
+        super(errorCode.getMessage(), cause);
+        this.errorCode = errorCode;
+    }
+}
