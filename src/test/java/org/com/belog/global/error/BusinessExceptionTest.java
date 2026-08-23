2bd0775b38ec38884573a8fe210fbc0c90bc9430
jyvnee <jyunee54@gmail.com>
2026-08-24 00:53:23 +0900

    test: 공통 예외 처리 테스트

--- src/test/java/org/com/belog/global/error/BusinessExceptionTest.java
+++ src/test/java/org/com/belog/global/error/BusinessExceptionTest.java
@@ -3,11 +3,14 @@ package org.com.belog.global.error;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.com.belog.global.response.code.CommonErrorCode;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("BusinessException 테스트")
 class BusinessExceptionTest {
 
     @Test
+    @DisplayName("에러 코드로 비즈니스 예외를 생성한다")
     void errorCodeCreatesBusinessException() {
         BusinessException exception = new BusinessException(CommonErrorCode.RESOURCE_NOT_FOUND);
 
@@ -16,6 +19,7 @@ class BusinessExceptionTest {
     }
 
     @Test
+    @DisplayName("원인 예외를 유지하면서 비즈니스 예외를 생성한다")
     void causeCreatesBusinessExceptionWithoutLosingOriginalException() {
         IllegalStateException cause = new IllegalStateException("original cause");
 
