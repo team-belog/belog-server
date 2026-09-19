75dbe6007eed8cd0cf0cdf5d657dc63592ddce06
jyvnee <jyunee54@gmail.com>
2026-09-20 04:25:40 +0900

    feat: 그룹 멤버 조회 repository 구현

--- src/main/kotlin/org/com/belog/group/repository/GroupMemberRepository.kt
+++ src/main/kotlin/org/com/belog/group/repository/GroupMemberRepository.kt
@@ -1,13 +1,34 @@
 package org.com.belog.group.repository
 
 import org.com.belog.group.domain.GroupMember
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface GroupMemberRepository : JpaRepository<GroupMember, Long> {
     fun countByGroupId(groupId: Long): Long
+
+    fun findByGroupIdAndUserId(
+        groupId: Long,
+        userId: Long,
+    ): GroupMember?
 
     fun existsByGroupIdAndUserId(
         groupId: Long,
         userId: Long,
     ): Boolean
+
+    @EntityGraph(attributePaths = ["user"])
+    @Query(
+        """
+        SELECT member
+        FROM GroupMember member
+        WHERE member.group.id = :groupId
+        ORDER BY member.role DESC, member.id ASC
+        """,
+    )
+    fun findAllWithUserByGroupId(
+        @Param("groupId") groupId: Long,
+    ): List<GroupMember>
 }
