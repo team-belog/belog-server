bda6c69d4f6d3b1b82985413b5006b52ad620fec
jyvnee <jyunee54@gmail.com>
2026-09-22 01:14:02 +0900

    feat: 일정 조율 현황 집계 서비스 구현

---
+++ src/main/kotlin/org/com/belog/meeting/service/result/MeetingDatePollResults.kt
@@ -0,0 +1,26 @@
+package org.com.belog.meeting.service.result
+
+import java.time.LocalDate
+
+data class MeetingDatePollResults(
+    val meetingId: Long,
+    val totalParticipantCount: Int,
+    val respondedParticipantCount: Int,
+    val unansweredMembers: List<DatePollMemberResult>,
+    val candidateDateResults: List<CandidateDatePollResult>,
+)
+
+data class DatePollMemberResult(
+    val groupMemberId: Long,
+    val nickname: String,
+)
+
+data class CandidateDatePollResult(
+    val candidateDateRangeId: Long,
+    val startDate: LocalDate,
+    val endDate: LocalDate,
+    val rank: Int,
+    val availableCount: Int,
+    val availableMembers: List<DatePollMemberResult>,
+    val unavailableMembers: List<DatePollMemberResult>,
+)
